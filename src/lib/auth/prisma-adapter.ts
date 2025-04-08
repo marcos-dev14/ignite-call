759733545499e3af2aa18e2963da222c7df6eb43
marcos-dev14 <mprogramador.10@gmail.com>
2025-04-08 10:35:18 -0300

    feat: conexão com calendário do google

---
+++ src/lib/auth/prisma-adapter.ts
@@ -0,0 +1,217 @@
+import { Adapter } from "next-auth/adapters"
+import { prisma } from "../prisma"
+import { parseCookies, destroyCookie } from "nookies"
+import { NextApiRequest, NextApiResponse } from "next"
+
+export function PrismaAdapter(
+  req: NextApiRequest,
+  res: NextApiResponse,
+): Adapter {
+  return {
+    async createUser(user) {
+      const { '@ignitecall:userId': userIdOnCookies } = parseCookies({ req })
+
+      if(!userIdOnCookies) {
+        throw new Error('User ID not found on cookies.')
+      }
+
+      const prismaUser = await prisma.user.update({
+        where: {
+          id: userIdOnCookies
+        },
+        data: {
+          name: user.name,
+          email: user.email,
+          avatar_url: user.avatar_url
+        }
+      })
+
+      destroyCookie({ res }, '@ignitecall:userId', {
+        path: '/',
+      })
+
+      return {
+        id: prismaUser.id,
+        name: prismaUser.name,
+        username: prismaUser.username,
+        email: prismaUser.email!,
+        emailVerified: null,
+        avatar_url: prismaUser.avatar_url!,
+      }
+    },
+    
+    async getUser(id) {
+      const user = await prisma.user.findUnique({
+        where: {
+          id
+        }
+      })
+
+      if (!user) return null
+
+      return { 
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        email: user.email!,
+        emailVerified: null,
+        avatar_url: user.avatar_url!,
+      }
+    },
+
+    async getUserByEmail(email) {
+      const user = await prisma.user.findUnique({
+        where: {
+          email
+        }
+      })
+
+      if (!user) return null
+
+      return { 
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        email: user.email!,
+        emailVerified: null,
+        avatar_url: user.avatar_url!,
+      }
+    },
+
+    async getUserByAccount({ providerAccountId, provider }) {
+      const account = await prisma.account.findUnique({
+        where: {
+          provider_provider_account_id: {
+            provider,
+            provider_account_id: providerAccountId
+          }
+        },
+        include: {
+          user: true,
+        }
+      })
+
+      if (!account) return null
+
+      return { 
+        id: account.user.id,
+        name: account.user.name,
+        username: account.user.username,
+        email: account.user.email!,
+        emailVerified: null,
+        avatar_url: account.user.avatar_url!,
+      }
+    },
+
+    async updateUser(user) {
+      const prismaUser = await prisma.user.update({
+        where: {
+          id: user.id
+        },
+        data: {
+          name: user.name,
+          email: user.email,
+          avatar_url: user.avatar_url
+        }
+      })
+
+      return { 
+        id: prismaUser.id,
+        name: prismaUser.name,
+        username: prismaUser.username,
+        email: prismaUser.email!,
+        emailVerified: null,
+        avatar_url: prismaUser.avatar_url!,
+      }
+    },
+
+    async linkAccount(account) {
+      await prisma.account.create({
+        data: {
+          user_id: account.userId,
+          type: account.type,
+          provider: account.provider,
+          provider_account_id: account.providerAccountId,
+          refresh_token: account.refresh_token,
+          access_token: account.access_token,
+          expires_at: account.expires_at,
+          token_type: account.token_type,
+          scope: account.scope,
+          id_token: account.id_token,
+          session_state: account.session_state
+        }
+      })
+    },
+
+    async createSession({ sessionToken, userId, expires }) {
+      await prisma.session.create({
+        data: {
+          user_id: userId,
+          expires,
+          session_token: sessionToken
+        },
+      })
+
+      return {
+        userId,
+        sessionToken,
+        expires
+      }
+    },
+
+    async getSessionAndUser(sessionToken) {
+      const sessionAndUser = await prisma.session.findUnique({
+        where: {
+          session_token: sessionToken
+        },
+        include: {
+          user: true,
+        }
+      })
+
+      if (!sessionAndUser) return null
+
+      return {
+        session: {
+          userId: sessionAndUser.user_id,
+          expires: sessionAndUser.expires,
+          sessionToken: sessionAndUser.session_token
+        },
+        user: {
+          id: sessionAndUser.user.id,
+          name: sessionAndUser.user.name,
+          username: sessionAndUser.user.username,
+          email: sessionAndUser.user.email!,
+          emailVerified: null,
+          avatar_url: sessionAndUser.user.avatar_url!,
+        }
+      }
+    },
+
+    async updateSession({ sessionToken, userId, expires }) {
+      const prismaSession = await prisma.session.update({
+        where: {
+          session_token: sessionToken
+        },
+        data: {
+          expires,
+          user_id: userId
+        }
+      })
+
+      return {
+        sessionToken: prismaSession.session_token,
+        userId: prismaSession.user_id,
+        expires: prismaSession.expires,
+      }
+    },
+
+    async deleteSession(sessionToken) {
+      await prisma.session.delete({
+        where: {
+          session_token: sessionToken
+        }
+      })
+    }
+  }
+}
--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,13 +1,24 @@
 import { Button, Heading, MultiStep, Text } from "@ignite-ui/react"
-import { signIn } from "next-auth/react"
-import { ArrowRight } from "phosphor-react"
+import { signIn, useSession } from "next-auth/react"
+import { useRouter } from "next/router"
+import { ArrowRight, Check } from "phosphor-react"
 
 import { Container, Header } from "../styles"
-import { ConnectBox, ConnectItem } from "./styles"
+import { AuthError, ConnectBox, ConnectItem } from "./styles"
+
 export default function Register() {
-  // async function handleRegister() {
-   
-  // }
+  const session = useSession()
+  const router = useRouter()
+  
+  const hasAuthErro = !!router.query.error
+  const isSignedIn = session.status === 'authenticated'
+
+  console.log(session)
+
+
+  async function handleConnectCalendar() {
+    await signIn('google')
+  }
 
 
   return (
@@ -34,17 +45,32 @@ export default function Register() {
             Google Calendar
           </Text>
 
-          <Button 
-            variant="secondary" 
-            size="sm"
-            onClick={() => signIn('google')}
-          >
-            Conectar 
-            <ArrowRight />
-          </Button>
+          {isSignedIn ? (
+            <Button size="sm" disabled>
+              Conectado
+              <Check />
+            </Button>
+          ) : (
+            <Button 
+              variant="secondary" 
+              size="sm"
+              onClick={handleConnectCalendar}
+            >
+              Conectar 
+              <ArrowRight />
+            </Button>
+          )}
+
         </ConnectItem>
 
-        <Button type="submit">
+        {hasAuthErro && (
+          <AuthError size="sm">
+            Falha ao se conectar ao Google, verifique se você habilitou as 
+            permissões de acesso ao Google Calendar
+          </AuthError>
+        )}
+
+        <Button type="submit" disabled={!isSignedIn}>
           Próximo passo
           <ArrowRight />
         </Button>
